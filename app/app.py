db4ea9214e85dcec4ca1d14ea6305cd177ca66ab
Jon <135755963+SoleP12@users.noreply.github.com>
2026-01-02 14:03:29 +0000

    imagekit problems

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
 from app.images import imagekit
-from imagekit.io.models.UploadFileRequestOptions import UploadFileRequestOptions
+from imagekitio.lib.requests.upload_file_request_options import UploadFileRequestOptions
 import shutil
 import uuid
 import os
@@ -32,26 +32,42 @@ async def upload_file(
     session: AsyncSession = Depends(get_async_session),
 ):
     # Create Object of Post 
+    # Create a temporary file to store the contents 
     temp_file_path = None
     try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix = os.path.split) as temp_file:
-            
+        with tempfile.NamedTemporaryFile(delete=False, suffix = os.path.splittext) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file,temp_file)
+        
+        upload_result = imagekit.upload_file(
+            file = open(temp_file_path, "rb"),
+            file_name = file.filename,
+            options = UploadFileRequestOptions(
+                use_unique_file_name = True,
+                tags=["backend-upload"]
+            ) 
+        )
+        if upload_result.respons_metadata_e.http_status_code == 200:
 
+            post = Post(
+                caption = caption,
+                url = upload_result.response.url,
+                file_type = file.content_type,
+                file_name = file.filename
+            )
 
+            # Adds post to database session, Commit the session, make sure to commit to session
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+    except Exception as e:
+        raise HTTPException(status_code = 500,detail = str(e) )
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
 
-
-    post = Post(
-        caption = caption,
-        url = "dummyurl",
-        file_type = "photo",
-        file_name = "dummy name"
-    )
-
-    # Adds post to database session, Commit the session, make sure to commit to session
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    return post
 
 
 @app.get("/feed")
--- app/images.py
+++ app/images.py
@@ -1,3 +1,4 @@
+
 from dotenv import load_dotenv
 from imagekitio import ImageKit
 import os
