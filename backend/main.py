6aa338df2c4ca0d6949838ace36c7412612d2332
Jon <135755963+SoleP12@users.noreply.github.com>
2026-01-03 00:07:00 +0000

    beginnings of connections

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
+
+
+@app.get("/")
+async def index(req: Request):
+    return templates.TemplateResponse(
+        name = "index.html",
+        context = {"request": req}
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")
