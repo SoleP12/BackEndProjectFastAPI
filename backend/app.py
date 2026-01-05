eacfb5ec29a9840b4e8ddb849611e6d361efeb4a
Jon <135755963+SoleP12@users.noreply.github.com>
2026-01-05 03:27:09 +0000

    Restart of Project creating catalogue system

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,9 @@ from pathlib import Path
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(
+    swagger_ui_parameters={"customCssUrl": "/static/custom.css"}
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
@@ -12,7 +14,7 @@ app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 
 @app.get("/")
-async def index(req: Request):
+async def TemplateRender(req: Request):
     return templates.TemplateResponse(
         name = "index.html",
         context = {"request": req}
