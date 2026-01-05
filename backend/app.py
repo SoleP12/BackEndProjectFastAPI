db9ae59e1e8e6b342492ba4c161ff2235c77790f
Jon <135755963+SoleP12@users.noreply.github.com>
2026-01-05 06:10:06 +0000

    adding more CRUD operations

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ import uvicorn
 
 # Database setup
 from tortoise.contrib.fastapi import register_tortoise
+from models import Supplier_Pydantic, SupplierIn_Pydantic, Supplier
 
 app = FastAPI(
     swagger_ui_parameters={"customCssUrl": "/static/custom.css?v=1.0.0"}
@@ -24,6 +25,37 @@ async def TemplateRender(req: Request):
         name = "index.html",
         context = {"request": req}
     )
+
+@app.post("/supplier")
+async def add_supplier(supplier_info: SupplierIn_Pydantic):
+    supplier_obj = await Supplier.create(**supplier_info.dict(exclude_unset = True)) 
+    response = await Supplier_Pydantic.from_tortoise_orm(supplier_obj)
+    return {"status": "ok", "data": response}
+
+
+@app.get("/supplier")
+async def get_all_suppliers():
+    response = await Supplier_Pydantic.from_queryset(Supplier.all())
+    return {"status": "ok", "data": response}
+
+@app.get("/supplier/{supplier_id}")
+async def get_specific_supplier(supplier_id: int):
+    response = await Supplier_Pydantic.from_queryset_single(Supplier.get(id = supplier_id))
+    return {"status": "ok", "data": response}
+
+@app.put("/supplier/{supplier_id}")
+async def updat_supplier(supplier_id: int, update_info: SupplierIn_Pydantic):
+    supplier = await Supplier.get(id = supplier_id)
+    update_info = update_info.dict(exclude_unset = True)
+    supplier.name = update_info["name"]
+    supplier.company = update_info["company"]
+    supplier.email = update_info["email"]
+    supplier.phone = update_info["phone"]
+    await supplier.save()
+    response = await Supplier_Pydantic.from_tortoise_orm(supplier)
+    return {"status": "ok", "data": response}
+
+
 
 # Database setup
 register_tortoise(
